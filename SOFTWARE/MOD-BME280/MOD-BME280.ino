0980411ee673c1f20e52b47e3f987c05577e3fcd
Stanimir-Petev <Stanimir.Petev@gmail.com>
2021-08-20 06:33:48 +0300

    Update
    I2C address changed from 0x76 to 0x77 to match the value defined by the default hardware position of the jumper.

--- SOFTWARE/MOD-BME280/MOD-BME280.ino
+++ SOFTWARE/MOD-BME280/MOD-BME280.ino
@@ -58,7 +58,7 @@ void setup() {
     
     // default settings
     // (you can also pass in a Wire library object like &Wire2)
-    status = bme.begin(0x76);  
+    status = bme.begin(0x77);  
     if (!status) {
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
         while (1);
